6b8c260d6c25f6344968a718a9a3c9243427cfd0
Lex <sss19960429@gmail.com>
2025-10-02 08:55:25 +0800

    [feat] swap 功能開發

--- swap/main.go
+++ swap/main.go
@@ -2,9 +2,52 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
-func swap[T any](a, b T) {}
+func swap[T any](a, b T) {
+	aVal := reflect.ValueOf(a)
+	bVal := reflect.ValueOf(b)
+
+	// 檢查是否為指標
+	if aVal.Kind() != reflect.Ptr {
+		panic("swap: first argument must be a pointer")
+	}
+	if bVal.Kind() != reflect.Ptr {
+		panic("swap: second argument must be a pointer")
+	}
+
+	// 檢查是否為 nil
+	if aVal.IsNil() {
+		panic("swap: first argument is nil")
+	}
+	if bVal.IsNil() {
+		panic("swap: second argument is nil")
+	}
+
+	// 取得指標指向的值
+	aElem := aVal.Elem()
+	bElem := bVal.Elem()
+
+	// 檢查是否可以設定值
+	if !aElem.CanSet() {
+		panic("swap: first argument cannot be set")
+	}
+	if !bElem.CanSet() {
+		panic("swap: second argument cannot be set")
+	}
+
+	// 檢查類型是否一致
+	if aElem.Type() != bElem.Type() {
+		panic(fmt.Sprintf("swap: type mismatch: %v != %v", aElem.Type(), bElem.Type()))
+	}
+
+	// 交換值
+	tmp := reflect.New(aElem.Type()).Elem()
+	tmp.Set(aElem)
+	aElem.Set(bElem)
+	bElem.Set(tmp)
+}
 
 func main() {
 	a := 10
